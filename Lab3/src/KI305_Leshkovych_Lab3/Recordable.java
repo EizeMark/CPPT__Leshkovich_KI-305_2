3442e5248990f64027b38dc934e06bca3e494fc0
Markiian <markiian.leshkovych.ki.2022@lpnu.ua>
2024-10-01 22:56:01 +0300

    Lab3

---
+++ Lab3/src/KI305_Leshkovych_Lab3/Recordable.java
@@ -0,0 +1,18 @@
+package KI305_Leshkovych_Lab3;
+
+/**
+ * iнтерфейс Recordable для функцiональностi запису вiдео.
+ */
+public interface Recordable {
+    /**
+     * Метод для запису вiдео.
+     *
+     * @param videoName Назва вiдео для запису.
+     */
+    void recordVideo(String videoName);
+
+    /**
+     * Метод для зупинки запису вiдео.
+     */
+    void stopRecording();
+}
